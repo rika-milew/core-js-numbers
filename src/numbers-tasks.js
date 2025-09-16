cf80ef88f0d96e91ca769c88228ec2504871b35a
Erika Milevskaya <rika.gumerova@gmail.com>
2025-09-16 23:04:12 +0300

    feat: implement isSafeInteger and roundToSmallestInteger function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -460,8 +460,8 @@ function getIntegerOnString(/* str, base */) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  return Number.isSafeInteger(number);
 }
 
 /**
@@ -474,8 +474,8 @@ function isSafeInteger(/* number */) {
  * 5.9  => 5
  * -5.1 => -6
  */
-function roundToSmallestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToSmallestInteger(number) {
+  return Math.floor(number);
 }
 
 /**
